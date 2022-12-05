4eea1f9ab346a2b72204b797db0cfb4d34d231e9
MaxTryhub <90391984+MaxTryhub@users.noreply.github.com>
2022-12-05 23:17:36 +0200

    myapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {Map} from "./componenets/Map/Map"
+import { useJsApiLoader } from "@react-google-maps/api";
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
+
+const defaultCenter = {
+  lat: 3.745,
+  lng: 38.523
+};
+console.log(defaultCenter)
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: API_KEY
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {isLoaded ? <Map center={defaultCenter}/> : <h2>Loading</h2>}
+
     </div>
   );
 }
